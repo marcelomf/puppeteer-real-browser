2eb497fe153837aff398cbddc914d15841cd9388
Marcelo Machado Fleury <marcelomf@gmail.com>
2024-05-09 08:44:29 -0300

    fix

--- index.js
+++ index.js
@@ -142,7 +142,7 @@ export const connect = ({
             await autoSolve({ page: page, browser: browserPptr })
         }
 
-        await page.setUserAgent(session.agent || session.userAgent || "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:127.0) Gecko/20100101 Firefox/127.0");        
+        //await page.setUserAgent(session.agent || session.userAgent || "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:127.0) Gecko/20100101 Firefox/127.0");        
 
         // await page.setViewport({
         //     width: 1920,
