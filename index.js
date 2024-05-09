c1fbe15a48d81f09021d6280488a0f6232f7a389
Marcelo Machado Fleury <marcelomf@gmail.com>
2024-05-09 11:11:36 -0300

    fix

--- index.js
+++ index.js
@@ -73,6 +73,14 @@ export const connect = ({
             protocol = "webDriverBiDi";
         }
 
+        var page = await browser.pages();
+        page = page[0];
+        await page.goto("https://www.bet365.com", { waitUntil: "networkidle0" });
+        await page.waitForSelector('body > div:nth-child(1) > div > div.wc-WebConsoleModule_SiteContainer > div.wc-WebConsoleModule_Header > div > div.hm-MainHeaderWide > div.hm-MainHeaderCentreWide > div:nth-child(2)', {timeout: 5000});
+        let aoVivo = await page.$('body > div:nth-child(1) > div > div.wc-WebConsoleModule_SiteContainer > div.wc-WebConsoleModule_Header > div > div.hm-MainHeaderWide > div.hm-MainHeaderCentreWide > div:nth-child(2)');
+        await aoVivo?.click();
+        return;
+
         browserPptr = await puppeteerExtra.connect({
             browser: (product == "firefox") ? browser : null,
             args: args,
